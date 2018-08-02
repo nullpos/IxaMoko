f99ced8f009f2e1eefcd252301cfdb43c34d71e3
nullpos <greenstar77@gmail.com>
2018-08-02 10:52:47 +0900

    白くじツールを色々修正

--- contents/ixa-white.js
+++ contents/ixa-white.js
@@ -3,21 +3,52 @@
         if (location.pathname != '/senkuji/senkuji.php') {
             return
         }
+        const WHITE_PRICE = 200
+        const STORAGE_KEY = "ixa_auto_white"
+        let timerId = -1
+        let storage = localStorage.getItem(STORAGE_KEY)
 
         let state = {
             'senkuji_token': null,
             'money': 0,
             'card': 0,
             'card_cap': 0,
-            'bids': 0
+            'bids': 0,
+            'statistics': [0, 0, 0, 0],
+            'percentage': [0, 0, 0],
+            white: function(result) {
+                state['card']++
+                state['money'] -= WHITE_PRICE
+                state['statistics'][0]++
+                state['statistics'][result['rare']-2]++
+                for(let i = 0; i < state['percentage'].length; i++) {
+                    if(state['statistics'][0] != 0) {
+                        state['percentage'][i] = parseInt(1000 * (state['statistics'][i+1] / state['statistics'][0])) / 10
+                    }
+                }
+            },
+            remove: function() {
+                state['card']--
+                state['money'] += 30
+            }
         }
-        const WHITE_PRICE = 200
-        const STORAGE_KEY = "ixa_auto_white"
-        let timerId = -1
 
         var xrwStatusText = function(xhr) {
             return xhr.setRequestHeader('X-Requested-With', 'statusText');
         };
+
+        try {
+            let tmp = JSON.parse(storage)
+            storage = tmp
+        } catch (error) {
+            storage = {
+                'while': 'while_card',
+                'while_num': '10',
+                'after': 'after_keep',
+                'keep_rare': 'toku',
+                'keep_skills': '槍隊堅守,槍隊進撃'
+            }
+        }
     
         // create interface
         
@@ -34,24 +65,25 @@
         let loop3 = createRadio('while_consume', 'while', '指定した銅銭を消費')
         let loopText = createTextInput('loopText', '指定数')
         loopDiv.append(loopInfo).append(loop1).append(loop2).append(loop3).append(loopText)
+        loopDiv.find('input[name=while]').val([storage['while']])
+        loopText.find('input').val(storage['while_num'])
 
         let afterDiv = $('<div style="display: inline-block; vertical-align: top; margin: 8px;"></div>')
         let afterInfo = $('<p><strong>後処理</strong></p>')
         let after1 = createRadio('after_remove', 'after', '破棄')
         let after2 = createRadio('after_keep', 'after', 'すべて保持')
         afterDiv.append(afterInfo).append(after1).append(after2)
+        afterDiv.find('input[name=after]').val([storage['after']])
 
         let keepDiv = $('<div style="display: inline-block; vertical-align: top; margin: 8px;"></div>')
         let keepInfo = $('<p><strong>保持する武将(破棄の場合のみ、いずれかに合致)</strong></p>')
         let keepRarity = createRaritySelect()
         let keepSkill = createTextInput('keepText', 'スキル名(カンマ区切り)')
         let keepSkillList = $('<p></p>')
-        if(localStorage.getItem(STORAGE_KEY)) {
-            let text = localStorage.getItem(STORAGE_KEY)
-            keepSkill.find('input:first').val(text)
-            showSkillList()
-        }
         keepDiv.append(keepInfo).append(keepRarity).append(keepSkill).append(keepSkillList)
+        keepRarity.find('select').val(storage['keep_rare'])
+        keepSkill.find('input:first').val(storage['keep_skills'])
+        showSkillList()
 
         let startDiv = $('<div style="text-align: right;"></div>')
         let startButton = $('<input type="button" value="開始" />')
@@ -69,6 +101,7 @@
         $('#ixaDogTop p').empty().append(text)
 
 
+        // event
         text.on('click', function(e) {
             div.toggle(400)
         })
@@ -78,10 +111,7 @@
         startButton.on('click', function() {
             if(timerId == -1) {
                 if(isValid()) {
-                    let skillText = $('#keepText').val()
-                    if(skillText.strip() != '') {
-                        localStorage.setItem(STORAGE_KEY, $('#keepText').val())
-                    }
+                    save()
                     startButton.val('中断')
                     init()
                     start()
@@ -95,6 +125,7 @@
             }
         })
 
+        // factory method
         function showSkillList() {
             let text = keepSkill.find('input:first').val()
             keepSkillList.html(text.split(',').join('</br>'))
@@ -122,7 +153,7 @@
             return div.append(label).append(select.append(options))
         }
 
-
+        // util
         function isValid() {
             let cond = $('input[name=while]:checked').val()
             let num = $('#loopText').val()
@@ -151,6 +182,18 @@
             } else {
                 return 0
             }
+        }
+
+        function save() {
+            storage['while'] = $('input[name=while]:checked').val()
+            storage['while_num'] = $('#loopText').val()
+            storage['after'] = $('input[name=after]:checked').val()
+            storage['keep_rare'] = $('#rarity').val()
+            let skills = $('#keepText').val()
+            if(skills.strip() != '') {
+                storage['keep_skills'] = skills
+            }
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(storage))
         }
 
 
@@ -240,18 +283,18 @@
                 card: function(data, removed) {
                     let rare = ''
                     if(data['rare'] <= 3) {
-                        rare = '特'
+                        rare = '<strong>特</strong>'
                     } else if(data['rare'] <= 4) {
                         rare = '上'
                     } else if(data['rare'] <= 5) {
                         rare = '序'
                     }
-                    let text = rare + ' No.' + data['no'] + ': ' + data['name'] + '(' + data['skill'] + ')'
+                    let text = ''
 
                     if(removed) {
-                        text += ' ...破棄</br>'
+                        text = rare + ' No.' + data['no'] + ': ' + data['name'] + '(' + data['skill'] + ') ...破棄</br>'
                     } else {
-                        text += ' ...保持</br>'
+                        text = rare + ' No.' + data['no'] + ': <strong>' + data['name'] + '</strong>(' + data['skill'] + ') ...保持</br>'
                     }
                     logs.html(text + logs.html())
                 }
@@ -263,6 +306,8 @@
             let cards = $('<p>所持カード: ' + state['card'] + '</p>')
             let bids = $('<p>入札中: ' + state['bids'] + '</p>')
             let card_cap = $('<p>カード上限: ' + state['card_cap'] + '</p>')
+            let statistics = $('<p></p>')
+
             let html_card = $('.l_cardstock')
             let html_money = $('.money_b')
             return {
@@ -271,11 +316,15 @@
                     cards.text('所持カード: ' + state['card'])
                     bids.text('入札中: ' + state['bids'])
                     card_cap.text('カード上限: ' + state['card_cap'])
+                    let text1 = '全体: ' + state['statistics'][0] + ', 特: ' + state['statistics'][1] + ', 上: ' + state['statistics'][2] + ', 序: ' + state['statistics'][3]
+                    let text2 = '(' + state['percentage'][0] + '%, ' + state['percentage'][1] + '%, ' + state['percentage'][2] + '%)'
+                    statistics.text(text1 + ' ' + text2)
+
                     html_card.text(state['card'] + ' / ' + state['card_cap'])
                     html_money.text(state['money'])
                 },
                 init: function() {
-                    displayDiv.append(money).append(cards).append(bids).append(card_cap)
+                    displayDiv.empty().append(money).append(cards).append(bids).append(card_cap).append(statistics).append('</br>')
                 }
             }
         })()
@@ -315,16 +364,13 @@
             }
 
             let result = white()
-
-            state['card']++
-            state['money'] -= WHITE_PRICE
+            state.white(result)
             display.update()
 
             if(after.shouldRemove(result)) {
                 remove(result['id'])
                 log.card(result, true)
-                state['card']--
-                state['money'] += 30
+                state.remove()
             } else {
                 log.card(result, false)
             }
@@ -332,6 +378,7 @@
             timerId = setTimeout(start, 500)
         }
 
+        // ajax
         function getBids() {
             let html = $.ajax({
                 type: 'get',
